ba64a8fe8accf0f44385ccc680b4657765a728b4
travelmail26 <travelmail26@gmail.com>
2023-06-19 09:54:52 -0600

    add ppg

--- mindmonitorppg.py
+++ mindmonitorppg.py
@@ -12,27 +12,27 @@ from matplotlib.animation import FuncAnimation
 import threading
 import csv
 from typing import List, Any
+import collections
 
-#Network Variables
+# Network Variables
 ip = "0.0.0.0"
 port = 5000
 
-#Muse Variables
+# Muse Variables
 hsi = [4,4,4,4]
 hsi_string = ""
 abs_waves = [-1,-1,-1,-1,-1]
 rel_waves = [-1,-1,-1,-1,-1]
 
-#Audio Variables
+# Audio Variables
 alpha_sound_threshold = 0.6
 sound_file = "bell.mp3"
 
-#Plot Array
-plot_val_count = 1500
+# Plot Array
+plot_val_count = 200
 plot_data = [[0],[0],[0],[0],[0]]
 raw_data = [0]
 
-import collections
 counter = collections.Counter()
 
 datapoints_received = 0
@@ -60,7 +60,7 @@ def print_data(address, *args):
         data_to_csv = []
 
 def raw_handler(address: str, *args):
-    #print ('raw handler triggered')
+    print ('raw handler triggered')
     global raw_data
     raw_data.append(args[0])
     #print('Raw EEG data: ', raw_data)
@@ -69,10 +69,18 @@ def raw_handler(address: str, *args):
 def hsi_handler(address: str,*args):
     global hsi, hsi_string
     hsi = args
-    if args[2]==1:  # Only check the status of the right forehead
-        hsi_string_new = "Muse Fit Good on Right Forehead"
+    if ((args[0]+args[1]+args[2]+args[3])==4):
+        hsi_string_new = "Muse Fit Good"
     else:
-        hsi_string_new = "Muse Fit Bad on Right Forehead"   
+        hsi_string_new = "Muse Fit Bad on: "
+        if args[0]!=1:
+            hsi_string_new += "Left Ear. "
+        if args[1]!=1:
+            hsi_string_new += "Left Forehead. "
+        if args[2]!=1:
+            hsi_string_new += "Right Forehead. "
+        if args[3]!=1:
+            hsi_string_new += "Right Ear."        
     if hsi_string!=hsi_string_new:
         hsi_string = hsi_string_new
         print(hsi_string) 
@@ -81,17 +89,25 @@ def abs_handler(address: str,*args):
     global hsi, abs_waves, rel_waves
     wave = args[0][0]
 
-    if hsi[2]==1:  # Only process the data if the right forehead fit is good
-        if len(args)==2: 
+    if (hsi[0]==1 or hsi[1]==1 or hsi[2]==1 or hsi[3]==1):
+        if (len(args)==2): 
             abs_waves[wave] = args[1] 
-        if len(args)==5: 
-            abs_waves[wave] = args[2]  # Only use the right forehead data
+        if (len(args)==5): 
+            sumVals=0
+            countVals=0            
+            for i in [0,1,2,3]:
+                if hsi[i]==1: 
+                    countVals+=1
+                    sumVals+=args[i+1]
+            abs_waves[wave] = sumVals/countVals
 
         rel_waves[wave] = math.pow(10,abs_waves[wave]) / (math.pow(10,abs_waves[0]) + math.pow(10,abs_waves[1]) + math.pow(10,abs_waves[2]) + math.pow(10,abs_waves[3]) + math.pow(10,abs_waves[4]))
         update_plot_vars(wave)
-        if wave==2 and len(plot_data[0])>10: 
+        if (wave==2 and len(plot_data[0])>10): 
             test_alpha_relative()
 
+def ppg_handler(address: str, *args):
+    print("PPG data received at {}: {}".format(address, args))
 
 def update_raw_data():
     global raw_data, plot_raw_data
@@ -134,18 +150,17 @@ def plot_update(i):
             waveLabel = 'Gamma'
         plt.plot(range(len(plot_data[wave])), plot_data[wave], color=colorStr, label=waveLabel+" {:.4f}".format(plot_data[wave][len(plot_data[wave])-1]))        
 
-    #print ('raw data in plot function**', raw_data, 'end raw***')
+    print ('raw data in plot function**', raw_data, 'end raw***')
     plt.plot(range(len(raw_data)), raw_data, color='brown', label='Raw EEG')
 
     plt.plot([0,len(plot_data[0])],[alpha_sound_threshold,alpha_sound_threshold],color='black', label='Alpha Sound Threshold',linestyle='dashed')
     plt.ylim([0,1])
-    plt.xticks(range(0, len(plot_data[0]), 100))
+    plt.xticks([])
     plt.title('Mind Monitor - Relative Waves and Raw EEG')
     plt.legend(loc='upper left')
 
 def init_plot():
     ani = FuncAnimation(plt.gcf(), plot_update, interval=100)
-    plt.gcf().set_size_inches(20, 10)
     plt.tight_layout()
     plt.show()
 
@@ -161,9 +176,10 @@ if __name__ == "__main__":
     dispatcher.map("/muse/elements/theta_absolute", abs_handler,1)
     dispatcher.map("/muse/elements/alpha_absolute", abs_handler,2)
     dispatcher.map("/muse/elements/beta_absolute", abs_handler,3)
-    #dispatcher.map("/muse/elements/gamma_absolute", abs_handler,4)
-    #dispatcher.map("/muse/eeg", raw_handler)
+    dispatcher.map("/muse/elements/gamma_absolute", abs_handler,4)
+    dispatcher.map("/muse/eeg", raw_handler)
+    dispatcher.map("/muse/ppg", ppg_handler)
 
-server = osc_server.ThreadingOSCUDPServer((ip, port), dispatcher)
-print("Listening on UDP port "+str(port))
-server.serve_forever()
+    server = osc_server.ThreadingOSCUDPServer((ip, port), dispatcher)
+    print("Listening on UDP port "+str(port))
+    server.serve_forever()
